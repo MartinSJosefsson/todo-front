acb193050d4a5e5a1a8222af38928bc6fb30d627
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-09-02 09:34:11 +0200

    add import axios

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,1 +1,19 @@
 //*todo: implement taskService and call the API
+
+import axios from 'axios';
+import {authService} from './authService';
+
+const API_URL = 'localhost9090/api'; // Replace with your API URL
+const config = {
+  headers: {
+    Authorization: `Bearer ${authService.getToken()}`,
+  },
+};
+
+export const getAllTodosApi = async () => {
+  const response = await axios.get(`${API_URL}/todo`, config);
+
+  const payload = response.data;
+  console.log(payload);
+  return payload;
+};
